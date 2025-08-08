2568c2ed0127628a11f53f9ddfab635686d1391e
Gordon999 <Gordon999@users.noreply.github.com>
2025-08-08 11:47:58 +0100

    V0.02 Update to gpiozero

--- 2B_MP3_Player.py
+++ 2B_MP3_Player.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import RPi.GPIO as GPIO
 import glob
 import subprocess
 import os, signal, sys
@@ -7,57 +6,61 @@ import time
 import random
 from random import shuffle
 from mutagen.mp3 import MP3
+from gpiozero import Button
+from gpiozero import LED
 
-#version 27102020_1
+#version 0.02
 
-# define physical GPIO pins used
+# define GPIOs used
 # BUTTONS
-PLAY = 35 # PLAY / STOP / SHUTDOWN (hold down for 5 seconds)
-PREV = 37 # PREVIOUS (whilst playing) / SHUFFLE (whilst stopped) / REBOOT (hold down for 5 seconds)
+PLAY = 19 # PLAY / STOP / SHUTDOWN (hold down for 5 seconds)
+PREV = 26 # PREVIOUS (whilst playing) / SHUFFLE (whilst stopped) / REBOOT (hold down for 5 seconds)
 # LEDS
-LED1 = 33 # READY LED / Flashes during startup checking for USB stick
-LED2 = 29 # PLAYING LED
-LED3 = 31 # SHUFFLED LED / LOADING MP3s(Flashes if no MP3s found)
+LED1 = 13 # READY LED / Flashes during startup checking for USB stick
+LED2 = 5  # PLAYING LED
+LED3 = 6  # SHUFFLED LED / LOADING MP3s(Flashes if no MP3s found)
 
 # setup GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-GPIO.setup(PLAY,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(PREV,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(LED1,GPIO.OUT)
-GPIO.setup(LED2,GPIO.OUT)
-GPIO.setup(LED3,GPIO.OUT)
-GPIO.output(LED1,GPIO.LOW) # turn LED1 OFF
-GPIO.output(LED2,GPIO.LOW) # turn LED2 OFF
-GPIO.output(LED3,GPIO.LOW) # turn LED3 OFF
+button_play = Button(PLAY)
+button_prev = Button(PREV)
+led_1 = LED(LED1)
+led_2 = LED(LED2)
+led_3 = LED(LED3)
+led_1.off()
+led_2.off()
+led_3.off()
+
+# find user
+h_user = "/home/" + os.getlogin( )
+m_user = "/media/" + os.getlogin( )
 
 # check if USB mounted
 start = time.time()
-usb = glob.glob("/media/pi/*")
+usb = glob.glob(m_user + "/*")
 if len(usb) < 1:
     while time.time() - start < 20 and len(usb) == 0:
-        usb = glob.glob("/media/pi/*")
-        GPIO.output(LED1,GPIO.LOW)
+        usb = glob.glob(m_user + "/*")
+        led_1.off()
         time.sleep(.25)
-        GPIO.output(LED1,GPIO.HIGH)
+        led_1.on()
         time.sleep(.25)
-GPIO.output(LED1,GPIO.LOW)
+led_1.off()
 
 # find MP3 tracks
 tracks  = []
 tracks2 = []
 while len(tracks) == 0:
-    GPIO.output(LED3,GPIO.LOW)
+    led_3.off()
     time.sleep(.25)
-    GPIO.output(LED3,GPIO.HIGH)
+    led_3.on()
     time.sleep(.25)
-    tracks = glob.glob("/home/pi/Music/*.mp3")
-    tracks2 = glob.glob("/media/pi/*/*/*/*.mp3")
+    tracks = glob.glob(h_user + "/Music/*.mp3")
+    tracks2 = glob.glob(m_user + "/*/*/*/*.mp3")
     for x in range(0,len(tracks2)):
         tracks.append(tracks2[x])
     tracks.sort()
-GPIO.output(LED3,GPIO.LOW)
-GPIO.output(LED1,GPIO.HIGH)
+led_3.off()
+led_1.on()
 
 # set starting variables
 Con_Play = 1
@@ -66,41 +69,41 @@ Z = 0
 shuffled = 0
 
 while True:
-    while GPIO.input(PLAY) == 1:
+    while not button_play.is_pressed:
         time.sleep(0.1)
         # check for shuffle key
-        if  GPIO.input(PREV) == 0:                     
+        if button_prev.is_pressed:                     
             time.sleep(0.5)
             if shuffled == 0:
                 print ("Shuffled")
                 shuffle(tracks)
                 shuffled = 1
-                GPIO.output(LED3,GPIO.HIGH)
+                led_3.on()
             else:
                 print ("unShuffled")
                 tracks.sort()
                 shuffled = 0
-                GPIO.output(LED3,GPIO.LOW)
+                led_3.off()
             timer1 = time.time()
-            while GPIO.input(PREV) == 0:
+            while button_prev.is_pressed:
                 # reboot if pressed > 5 seconds
                 if time.time() - timer1 > 5:                  
-                    GPIO.output(LED2,GPIO.LOW)
-                    GPIO.output(LED1,GPIO.LOW)
-                    GPIO.output(LED3,GPIO.LOW)
+                    led_2.off()
+                    led_1.off()
+                    led_3.off()
                     print ("Restart")
                     os.system("reboot")
     timer1 = time.time()
-    while GPIO.input(PLAY) == 0:
+    while button_play.is_pressed:
         # shutdown if pressed > 5 seconds
         if time.time() - timer1 > 5:                  
-            GPIO.output(LED2,GPIO.LOW)
-            GPIO.output(LED1,GPIO.LOW)
-            GPIO.output(LED3,GPIO.LOW)
+            led_2.off()
+            led_1.off()
+            led_3.off()
             print ("Goodbye!")
             os.system("sudo shutdown -h now")
     Con_Play = 1
-    GPIO.output(LED2,GPIO.HIGH)                      
+    led_2.on()                      
     
     while Con_Play == 1 :
         rpistr = "mplayer " + " -quiet " +  '"' + tracks[Z] + '"'
@@ -118,34 +121,34 @@ while True:
                 os.killpg(p.pid, signal.SIGTERM)
                 time.sleep(1)
             # check for stop/shutdown
-            if  GPIO.input(PLAY) == 0:                     
+            if button_play.is_pressed:                    
                 timer1 = time.time()
                 print ("Track stopped")
                 os.killpg(p.pid, signal.SIGTERM)
                 Con_Play = 0
-                GPIO.output(LED2,GPIO.LOW)
-                while GPIO.input(PLAY) == 0:
+                led_2.off()
+                while button_play.is_pressed:
                     # shutdown if pressed > 5 seconds
                     if time.time() - timer1 > 5:       
-                        GPIO.output(LED2,GPIO.LOW)
-                        GPIO.output(LED1,GPIO.LOW)
-                        GPIO.output(LED3,GPIO.LOW)  
+                        led_2.off()
+                        led_1.off()
+                        led_3.off()  
                         print ("Goodbye!")
                         os.killpg(p.pid, signal.SIGTERM)
                         os.system("sudo shutdown -h now")
             # check for previous key 
-            elif  GPIO.input(PREV) == 0:                   
+            elif button_prev.is_pressed:                   
                 print ("Previous Track...")
                 time.sleep(0.5)
                 os.killpg(p.pid, signal.SIGTERM)
                 Z -= 2
                 timer1 = time.time()
-                while GPIO.input(PREV) == 0:
+                while button_prev.is_pressed:
                     # reboot if pressed > 5 seconds
                     if time.time() - timer1 > 5:                  
-                        GPIO.output(LED2,GPIO.LOW)
-                        GPIO.output(LED1,GPIO.LOW)
-                        GPIO.output(LED3,GPIO.LOW)
+                        led_2.off()
+                        led_1.off()
+                        led_3.off()
                         print ("Restart")
                         os.system("reboot")
             poll = p.poll()
